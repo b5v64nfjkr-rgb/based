2b3cdab2c27e0e9a2dc60f44eb6d15f7bfce33bd
b5v64nfjkr-rgb <b5v64nfjkr@privaterelay.appleid.com>
2025-11-19 22:12:23 -0500

    Delete furniture-store directory

--- furniture-store/app/(site)/about/page.tsx
+++
@@ -1,8 +0,0 @@
-export default function About() {
-  return (
-    <main className="container about">
-      <h1>About Modern Furniture</h1>
-      <p>We design and curate modern furniture for well-lived homes.</p>
-    </main>
-  )
-}
